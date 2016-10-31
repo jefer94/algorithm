bb2baeb78bd78b80294e029217f02b73fae2f127
Jeferson De Freitas <jdefreitaspinto@gmail.com>
2016-10-30 23:11:18 -0400

    some changue, and more commentary

--- core/algorithm.js
+++ core/algorithm.js
@@ -10,15 +10,22 @@ var tokens = {
 
 class Algorithm {
   constructor() {
-
+    this.js = "";
   }
+  // transform to javascript
   to_js(event) {
+    // if event not exist or the method keyCode from event is equal at 13
     if (typeof event == "undefined" || event.keyCode == 13) {
+      // get container result in a var
       var result = document.getElementById("result");
+      // get container of box executor
       var execute = document.getElementById("execute");
 
+      // add this value in the log
       result.innerHTML += execute.value + "<br>";
+      // and execute a interpreter
       this.scanner();
+      // to finish to clean execute box
       execute.value = "";
 
       return false;
@@ -27,15 +34,32 @@ class Algorithm {
       return true;
     }
   }
+  // transform between native languaje and javascipt
   scanner() {
+    // each word is separated into a array
     var word = execute.value.split(" ");
-    console.log(word);
+    //console.log(word);
+    // this loop is to search in various dictionaries, and transform that code
     for (var i in word) {
-      if (transpiler[word[i]]) console.log(transpiler[word[i]]);
-      else if (tokens[word[i]]) console.log(tokens[word[i]]);
-      else console.log(word[i]);
+      // dictionaries of words
+      if (transpiler[word[i]])
+        this.js += transpiler[word[i]] + " ";
+      // dictionaries of tokens
+      else if (tokens[word[i]])
+        this.js += tokens[word[i]] + " ";
+      // and words not in the dictionary
+      else
+        this.js += word[i] + " ";
     }
+    this.js += "<br>";
+    // get container result in a var
+    var result = document.getElementById("result");
+    // add this value in the log
+    result.innerHTML = this.js;
+
+    console.log(this.js);
   }
 }
 
+// instance of mechanic
 var algorithm = new Algorithm;
