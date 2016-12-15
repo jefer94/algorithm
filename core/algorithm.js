4355d46f1f5a1502e6b334618fe5610b496efd2a
Jeferson De Freitas <jdefreitaspinto@gmail.com>
2016-12-15 03:45:10 -0400

    rework of transpiler in progress

--- core/algorithm.js
+++ core/algorithm.js
@@ -25,50 +25,54 @@ class Algorithm {
     this.js = "";
   }
   // transform to javascript
-  to_js(event) {
-    // if event not exist or the method keyCode from event is equal at 13
-    if (typeof event == "undefined" || event.keyCode == 13) {
-      // get container result in a var
-      var result = document.getElementById("result");
-      // get container of box executor
-      var execute = document.getElementById("execute");
+  to_js() {
+    // get container result in a var
+    this.code = document.getElementById("code");
+    // get container of box executor
+    this.console = document.getElementById("console");
 
-      // add this value in the log
-      result.innerHTML += execute.value + "<br>";
-      // and execute a interpreter
-      this.scanner();
-      // to finish to clean execute box
-      execute.value = "";
-
-      return false;
-    }
-    else {
-      return true;
-    }
+    // add this value in the log
+    //result.innerHTML += execute.value + '<div class="lines"><div class="CodeMirror-linenumber CodeMirror-gutter-elt arrow">&gt;</div> <div class="margin-line"> algorithm run unname.js</div></div>'
+    // and execute a interpreter
+    this.scanner();
+    // to finish to clean execute box
+    //execute.value = "";
   }
   // transform between native languaje and javascipt
   scanner() {
-    // each word is separated into a array
-    var word = execute.value.split(" ");
-    //console.log(word);
-    // this loop is to search in various dictionaries, and transform that code
-    for (var i in word) {
-      // dictionaries of words
-      if (transpiler[word[i]])
-        this.js += transpiler[word[i]] + " ";
-      // dictionaries of tokens
-      else if (tokens[word[i]])
-        this.js += tokens[word[i]] + " ";
-      // and words not in the dictionary
-      else
-        this.js += word[i] + " ";
+    // each line is separated into a array
+    var line = this.code.value.split("\n");
+    for (i in line){
+      // each word is separated into a array
+      var word = line[i].split(" ");
+      console.log(word);
+      // this loop is to search in various dictionaries, and transform that code
+      for (var i in word) {
+        // dictionaries of words
+        if (transpiler[word[i]])
+          this.js += transpiler[word[i]] + " ";
+        // dictionaries of tokens
+        else if (tokens[word[i]])
+          this.js += tokens[word[i]] + " ";
+        // and words not in the dictionary
+        else
+          this.js += word[i] + " ";
+      }
+      if (word[word.length-1].search("{") != -1 || word[word.length-1].search("}") != -1) {
+        this.js += "\n";
+        console.log("a");
+      }
+      else if (word[0].search("mostrar") != -1 || word[0].search("imprimir") != -1) {
+        this.js = this.js.replace("mostrar", "write(");
+        this.js = this.js.replace("imprimir", "write(");
+        this.js += ");\n";
+        console.log("b");
+      }
+      else {
+        this.js += ";\n";
+        console.log("c");
+      }
     }
-    this.js += "<br>";
-    // get container result in a var
-    var result = document.getElementById("result");
-    // add this value in the log
-    result.innerHTML = this.js;
-
     console.log(this.js);
   }
 }
