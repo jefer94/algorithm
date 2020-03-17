2099b4d0deecc9d60452e5a273fadf02e75971e1
jefer94 <jdefreitaspinto@gmail.com>
2020-03-17 00:03:13 -0500

    purge dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,7 @@
-// const rewireEslint = require('react-app-rewire-eslint')
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
-  return { ...config, plugins: [...config.plugins, new MonacoWebpackPlugin()] };
+  // return { ...config, plugins: [...config.plugins, new MonacoWebpackPlugin()] };
+  return config
 }
