5a9a0ca8b98de42341718d2b2a82a2eaf1bbfbb0
jefer94 <jdefreitaspinto@gmail.com>
2020-03-18 00:00:45 -0500

    add example component

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -1,7 +1,64 @@
 import React from 'react'
+import { IonSearchbar, IonList, IonItem, IonLabel, IonButton, IonListHeader, IonGrid, IonRow, IonCol } from '@ionic/react'
+import './Docs.sass'
+import keychain from '../libs/keychain'
+
+function addDoc(name, description, content) {
+  return {
+    id: keychain('doc'),
+    name,
+    description,
+    content
+  }
+}
 
 export default function () {
+  const docs = [
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa'),
+    addDoc('hola', 'asdasdsaddsasddsadsadaddsaa')
+  ]
   return (
-    <></>
+    <div className="algorithm-docs-wrapper">
+      <IonSearchbar mode="ios" />
+      <IonGrid>
+        <IonRow>
+          <IonCol size-xs="12" size-lg="6">
+            <IonList>
+              <IonListHeader mode="ios">Save</IonListHeader>
+              {docs.map(({ id, name, description }) => (
+                <IonItem key={id}>
+                  <IonLabel>
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                  </IonLabel>
+                  {/* <IonButton slot="end" color="tertiary">Delete</IonButton> */}
+
+                  <IonButton slot="end">Load</IonButton>
+                </IonItem>
+              ))}
+            </IonList>
+          </IonCol>
+          <IonCol size-xs="12" size-lg="6">
+            <IonList>
+              <IonListHeader mode="ios">Tutorial</IonListHeader>
+              {docs.map(({ id, name, description }) => (
+                <IonItem key={id}>
+                  <IonLabel>
+                    <h1>{name}</h1>
+                    <p>{description}</p>
+                  </IonLabel>
+                  <IonButton slot="end">Load</IonButton>
+                </IonItem>
+              ))}
+            </IonList>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,64 @@
 import React, { lazy, Suspense } from 'react'
 import { render } from 'react-dom'
+import { IonApp } from '@ionic/react'
+// import { IonApp, IonRouterOutlet } from '@ionic/react'
+// import { IonReactRouter } from '@ionic/react-router'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-// import { Provider } from 'react-redux'
+// import { Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker'
+import Loading from './components/Loading'
+
+import './index.sass'
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css'
+// import '@ionic/react/css/structure.css'
+import '@ionic/react/css/typography.css'
+
+import '@ionic/react/css/padding.css'
+import '@ionic/react/css/float-elements.css'
+import '@ionic/react/css/text-alignment.css'
+import '@ionic/react/css/text-transformation.css'
+import '@ionic/react/css/flex-utils.css'
+import '@ionic/react/css/display.css'
+
 import './globals/variables.sass'
 import './globals/buttons.sass'
 
-// import Docs from './containers/Docs'
-// import Console from './containers/Console'
-// import TabsAndEditor from './containers/TabsAndEditor'
-import Loading from './components/Loading'
+import './sass/onedark.sass'
+import './sass/editor.sass'
+
+
+// import './index.sass'
 
 const Docs = lazy(() => import('./containers/Docs'))
 const Console = lazy(() => import('./containers/Console'))
 const TabsAndEditor = lazy(() => import('./containers/TabsAndEditor'))
 
-
-// import 'codemirror/lib/codemirror.css'
-// import 'codemirror/addon/hint/show-hint.css'
-import './sass/onedark.sass'
-import './sass/editor.sass'
-
 render(
-  <Router>
-    <Suspense fallback={<Loading />}>
-      <Switch>
-        <Route component={TabsAndEditor} path="/" exact />
-        <Route component={Docs} path="/docs" exact />
-        <Route component={Console} path="/console" exact />
-      </Switch>
-    </Suspense>
-  </Router>,
+  // <IonApp>
+  //   <IonReactRouter>
+  //     <Suspense fallback={<Loading />}>
+  //       <IonRouterOutlet>
+  //         <Route component={TabsAndEditor} path="/" exact />
+  //         <Route component={Docs} path="/docs" exact />
+  //         <Route component={Console} path="/console" exact />
+  //       </IonRouterOutlet>
+  //     </Suspense>
+  //   </IonReactRouter>
+  // </IonApp>,
+  <IonApp>
+    <Router>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route component={TabsAndEditor} path="/" exact />
+          <Route component={Docs} path="/docs" exact />
+          <Route component={Console} path="/console" exact />
+        </Switch>
+      </Suspense>
+    </Router>
+  </IonApp>,
   document.getElementById('root')
 )
 
@@ -40,3 +67,4 @@ render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 if (window.location.protocol === 'file:') serviceWorker.unregister()
 else serviceWorker.register()
+// serviceWorker.register()
